276aeec326411d105e2c8432144694a1506e36c9
Gökhan atan <gkhnatn@Gokhan-MacBook-Pro.local>
2020-12-12 22:23:34 +0300

    Added getall and add on Product.API

--- Product.API/Controllers/ProductsController.cs
+++ Product.API/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Product.API.Controllers
@@ -6,8 +7,32 @@ namespace Product.API.Controllers
     [Route("api/products")]
     public class ProductsController:ControllerBase
     {
+         private List<Models.Product> _products = new List<Models.Product>();
+
+        public void InitializeCategories()
+        {
+            _products.Add(new Models.Product() { Id = 1, Name = "Apple Laptop", Quantity = 2 });
+            _products.Add(new Models.Product() { Id = 2, Name = "IPhone 12" , Quantity = 20 });
+            _products.Add(new Models.Product() { Id = 3, Name = "IPhone 11 Pro", Quantity = 15  });
+            _products.Add(new Models.Product() { Id = 4, Name = "Apple Magic Mouse 2" , Quantity = 10 });
+            _products.Add(new Models.Product() { Id = 5, Name = "Dell Monitor", Quantity = 12  });
+        }
         public ProductsController()
         {
+            InitializeCategories();
         }
+
+        [Route("getall")]
+        public IActionResult GetAll()
+        {
+            return Ok(_products);
+        }
+
+        public IActionResult Add(Models.Product product)
+        {
+            _products.Add(product);
+            return Ok("Product added.");
+        }
+
     }
 }
---
+++ Product.API/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace Product.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        
+    }
+}
